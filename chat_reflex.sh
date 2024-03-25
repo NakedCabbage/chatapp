270aa6a2f4b925b0728fca76a7116f93a55ef9c2
Jeffery <140289025+NakedCabbage@users.noreply.github.com>
2024-03-25 01:10:44 +0000

    new added history

--- chat_reflex.sh
+++ chat_reflex.sh
@@ -4,11 +4,13 @@ cd /
 ls 
 cd ~/CMD 
 pwd
-scp  /path/to/local/folder username@remote_host:/path/to/destination
+# scp  /path/to/local/folder username@remote_host:/path/to/destination
 
-scp -r david@34.42.194.88:/home/chatapp/chatapp/ ./chatapp  
-scp -r ./* david@34.42.194.88:/home/david/
+# scp -r david@34.42.194.88:/home/chatapp/chatapp/      ./chatapp 
+scp -r ./chatapp/   david@34.42.194.88:/home/chatapp/
+
 # login into 34.16.19.16 and run 
-ssh -t david@34.16.19.16 "cd ../; cd chatapp;sudo reflex run;"
+ssh -t david@34.42.194.88  "cd ../; cd chatapp;sudo reflex run;"
 
-new = 34.42.194.88 
+# scp: stat remote: No such file or directory
+# scp: failed to upload directory ./chatapp to /home/chatapp/chatapp/
