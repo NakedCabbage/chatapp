581cce5ac98d1cfc78656ac2cc9cc3022d074416
Jeffery <140289025+NakedCabbage@users.noreply.github.com>
2024-03-27 01:42:09 +0000

    chat_reflex_working

--- chat_reflex.sh
+++ chat_reflex.sh
@@ -1,16 +1,22 @@
-# ssh david@34.16.19.16
-ls
-cd /
-ls 
-cd ~/CMD 
-pwd
-# scp  /path/to/local/folder username@remote_host:/path/to/destination
+# save this $private_key into a file call "id_rsa" inside  /home/runner/.ssh
+
+#echo $private_key --------------------------------------
+# translate python code to shell script 
+
+# save the private key to /home/runner/.ssh from the secrets
+
+# echo "$private_key" > /home/runner/.ssh/id_rsa
+# chmod 600 /home/runner/.ssh/id_rsa
 
 # scp -r david@34.42.194.88:/home/chatapp/chatapp/      ./chatapp 
-scp -r ./chatapp/   david@34.42.194.88:/home/chatapp/
+echo test1234 |scp -r ./chatapp/   david@34.71.27.28:/home/david/chatapp/
 
 # login into 34.16.19.16 and run 
-ssh -t david@34.42.194.88  "cd ../; cd chatapp;sudo reflex run;"
+echo test1234|ssh -t david@34.71.27.28  "cd ~/chatapp;sudo reflex run;"
 
 # scp: stat remote: No such file or directory
 # scp: failed to upload directory ./chatapp to /home/chatapp/chatapp/
+# cmd1 | cmd2 
+# sends the ouput of cmd1 into the input of cmd2
+# cmd1 > file
+# sending the output of cmd1, to the file 
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+      pkgs.nano
       pkgs.openssh_gssapi
       pkgs.cowsay
     ];
