21b871514b5d29ada0312cc80f32aed3e8f1ea30
Jeffery <140289025+NakedCabbage@users.noreply.github.com>
2024-04-08 01:22:46 +0000

    added mkdir.ssh

--- chat_reflex.sh
+++ chat_reflex.sh
@@ -4,13 +4,13 @@
 # translate python code to shell script 
 
 # save the private key to /home/runner/.ssh from the secrets
-
+mkdir .ssh 
 echo "$private_key_64" | openssl base64 -A -d > /home/runner/.ssh/id_rsa 
 chmod 600 /home/runner/.ssh/id_rsa
 
 # scp -r david@34.42.194.88:/home/chatapp/chatapp/      ./chatapp 
-scp -r ./chatapp/   david@34.16.207.246:/home/david/chatapp/
+scp -r ./chatapp/   david@34.125.7.139:/home/david/chatapp/
 
 # login and run 
-ssh -t david@34.16.207.246  " source venv/bin/activate; cd ~/chatapp;  reflex run;"
+ssh -t david@34.125.7.139  " source venv/bin/activate; cd ~/chatapp;  reflex run;"
 
